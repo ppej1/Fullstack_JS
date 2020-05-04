460e10fdf284387c1ec61d5c9a6224bf6712737f
ppej1 <xodnjs1016@gmail.com>
2020-05-04 21:46:09 +0900

    동영상 업로드 완료

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const PORT = 4000;
 //middleware 
 app.use(helmet());
 app.set("view engine", "pug")
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
