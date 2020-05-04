233e8411ef1f3d7afbd558fe5421419a92107a73
ppej1 <xodnjs1016@gmail.com>
2020-05-04 23:20:10 +0900

    edit video 완료

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { postUpload, getUpload, videoDetail, editVideo,  deleteVideo } from "../controllers/videoController";
+import { postUpload, getUpload, videoDetail, getEditVideo,postEditVideo,  deleteVideo } from "../controllers/videoController";
 import {uploadVideo} from "../middlewares";
 
 
 const videoRouter = express.Router();
 
+
+//upload
 videoRouter.get(routes.upload,getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+//video Detail
 videoRouter.get(routes.videoDetail(),videoDetail);
-videoRouter.get(routes.editVideo,editVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(),getEditVideo);
+videoRouter.post(routes.editVideo(),postEditVideo);
+//Delete Video
 videoRouter.get(routes.deleteVideo,deleteVideo);
 
 
