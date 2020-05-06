bb207569d2d32fd3402cf1faaef8b7003cbfb61d
ppej1 <xodnjs1016@gmail.com>
2020-05-06 18:56:26 +0900

    webkit 설치 1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cookieParser from"cookie-parser";
+import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {localsMiddleware} from "./middlewares";
+import {
+    localsMiddleware
+} from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -16,18 +18,21 @@ const PORT = 4000;
 app.use(helmet());
 app.set("view engine", "pug")
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 
 
 //router
-app.use(routes.home , globalRouter);
-app.use(routes.users , userRouter);
-app.use(routes.videos , videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 
 export default app;
